ade8e32a07df518dcc1722608ef21bb8dec859e3
yusran.cuper <yusran.cuper@gmail.com>
2019-12-02 16:22:55 +0800

    tambah list di index.html:

--- index.php
+++ index.php
@@ -8,5 +8,11 @@
 </head>
 <body>
     <h1>test Git aja</h1>
+    <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Exercitationem, doloribus non? Itaque in ad quo perferendis, nihil fugiat debitis sapiente distinctio quidem nesciunt architecto iure repudiandae culpa magnam dignissimos explicabo.s</p>
+<h2> Data lama :</h2>
+<ul>
+<li>test1</li>
+<li>test2</li>
+</ul>
 </body>
 </html>
